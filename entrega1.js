e24543cd14b4cf100f78c03f392e222940099b27
jachaval <slash.glp@gmail.com>
2023-04-25 00:02:30 -0300

    alpha

--- entrega1.js
+++ entrega1.js
@@ -7,7 +7,7 @@ class ProductManager {
         return this.products;
     }
 
-    addProduct = (title, description, price, thumbnail, code, stock) =>{
+    addProduct = (title = "producto prueba", description = "Este es un producto prueba", price = 200, thumbnail = "no image", code = "abc123", stock= 25) =>{
         const product = {
             title,
             description,
