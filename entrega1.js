952b397b50f5785de92541ab865381b931f14a5b
jachaval <slash.glp@gmail.com>
2023-04-25 00:19:18 -0300

    alpha

--- entrega1.js
+++ entrega1.js
@@ -16,6 +16,10 @@ class ProductManager {
             code,
             stock                
         }
+        if (this.products.length===0){
+            product.id = 1;
+        }
+        else {product.id = this.products [this.products.length - 1].id + 1}
 
     }
    
