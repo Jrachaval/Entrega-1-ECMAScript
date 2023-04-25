3152f1e8606781dd4182e48e2b7303da7179cf8e
jachaval <slash.glp@gmail.com>
2023-04-25 18:17:00 -0300

    alpha

--- entrega1.js
+++ entrega1.js
@@ -7,7 +7,7 @@ class ProductManager {
         return this.products;
     }
 
-    addProduct = (title = "producto prueba", description = "Este es un producto prueba", price = 200, thumbnail = "no image", code = "abc123", stock= 25) =>{
+    addProduct = (title, description , price , thumbnail , code , stock) =>{
         const product = {
             title,
             description,
@@ -19,9 +19,16 @@ class ProductManager {
         if (this.products.length===0){
             product.id = 1;
         }
-        else {product.id = this.products [this.products.length - 1].id + 1}
-
+        else {product.id = this.products [this.products.length - 1].id + 1};  
+                 
+        this.products.push(product);
+    
     }
+    
+    
    
 }
 
+const manejadorproductos = new ProductManager()
+manejadorproductos.addProduct("producto prueba", "este es un producto prueba", 200, "no image", "abc123", 25)
+console.log(manejadorproductos.getProducts());
